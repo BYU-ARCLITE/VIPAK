24f3a745adca4bc7e419733f234293adc4ffa5a5
Paul D. Handy <paul.d.handy@gmail.com>
2012-07-03 15:35:27 -0600

    working on hotkeys

--- ipakeyboard.js
+++ ipakeyboard.js
@@ -18,7 +18,34 @@ $(function(){
     cE = cS;
     field.caret(cE,cE);
   });
+  $('input[type=text],input[type=textbox],textarea').keydown(function(e){
+    console.log(String.fromCharCode(e.keyCode)+":"+e.which);
+  });
 });
+/*keyCode: {
+		BACKSPACE: 8,
+		COMMA: 188,
+		DELETE: 46,
+		DOWN: 40,
+		END: 35,
+		ENTER: 13,
+		ESCAPE: 27,
+		HOME: 36,
+		LEFT: 37,
+		NUMPAD_ADD: 107,
+		NUMPAD_DECIMAL: 110,
+		NUMPAD_DIVIDE: 111,
+		NUMPAD_ENTER: 108,
+		NUMPAD_MULTIPLY: 106,
+		NUMPAD_SUBTRACT: 109,
+		PAGE_DOWN: 34,
+		PAGE_UP: 33,
+		PERIOD: 190,
+		RIGHT: 39,
+		SPACE: 32,
+		TAB: 9,
+		UP: 38
+	}*/
 function KeyboardFactory(){
   this.fullkeyboard = function(){
     var k;
